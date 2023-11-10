967eae83cfd446a880362bf8cf00a7f32b0ce7b2
Yuvraj Singh <yuvrajsinghrandhawaishere@gmail.com>
2023-11-10 15:09:36 -0600

    did js and gave final touches to the page

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,2 +1,29 @@
 'use strict';
 
+const knowMoreButton = document.querySelector('.center section div a');
+const aboutMeSection = document.querySelector('.center h2');
+const contents = [
+    "Currently studying software developer at MITT, Winnipeg",
+    "I am passionate about learning and creating awesome websites!",
+    "Do you want to know more? Get in touch. I look forward to hearing from you"
+];
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let currentState = 0;
+    let clickCount = 0;
+
+    knowMoreButton.addEventListener('click', function () {
+        aboutMeSection.textContent = contents[currentState];
+        currentState = (currentState + 1) % contents.length;
+        
+        clickCount++;
+
+        if (clickCount === 3) {
+            knowMoreButton.textContent = 'Again?';
+        } else if (clickCount === 4) {
+            clickCount = 0;
+            knowMoreButton.textContent = 'Know more';
+        }
+    });
+});
